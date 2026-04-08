d670f6998d9029792d4d6b8ab57bcab9c69591d4
commonrover64 <siddhartharampalsingh@gmail.com>
2026-04-08 17:52:50 +0530

    mounted authroutes to server

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,11 @@ const migrate = require("./migrations/migrate");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const authRoutes = require("./src/modules/auth/auth.routes");
 
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 migrate().then(() => {
   app.listen(PORT, () => {
